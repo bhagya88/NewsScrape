1669b1be19cebcf5b4aafc61c9d1c0776021488e
bhagya <bhagya@outlook.com>
2016-12-16 21:30:31 -0600

    added routes

--- models/Article.js
+++ models/Article.js
@@ -12,7 +12,7 @@ var ArticleSchema = new Schema({
 
 	} ,
 	note:{
-		type:Schema.Types.objectId,
+		type: Schema.Types.ObjectId,
 		ref: 'Note'
 
 	}
--- server.js
+++ server.js
@@ -1,23 +1,32 @@
+// get all dependencies
 var express = require('express');
 var mongoose = require('mongoose');
 var cheerio = require('cheerio');
-var bodyParser = require('bodyParser');
+var bodyParser = require('body-parser');
 var path = require('path');
 var methodOverride = require('method-override');
 var request = require('request');
-var exphbs = require('express-handlebars');
 var Promise = require('bluebird');
-var logger = require('logger');
+var logger = require('morgan');
+
+var articleRouter = require('./routes/articleroute.js');
+
+
 var PORT = process.env.PORT || '3000';
-var Article = require('./models/Article.js');
-var Note = require('./models/Note.js');
+
 
 mongoose.Promise = Promise;
 // Database configuration with mongoose
 mongoose.connect("mongodb://localhost/newscrape");
 var db = mongoose.connection;
 
-db.on()
+db.on('error',function(error){
+	console.log('Mongoose error',error);
+})
+
+db.once('open',function(){
+ 	console.log('Mongoose connection successful');
+});
 
 var app = express();
 
@@ -26,8 +35,7 @@ app.use(bodyParser.urlencoded({
 	extended: false 
 }));
 
-
-
-
+app.use(express.static('public'));
+app.use('/',articleRouter);
 
 app.listen(PORT);
